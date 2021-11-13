8d4e68afdbe31867f8ba8d1fb189deed012c34e5
Ellyzeul <gabrielaug.240@gmail.com>
2021-11-13 17:59:19 -0300

    chore: adding CORS policy

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ if(!isset($_ENV["DB_NAME"])) {
     $dotenv = Dotenv\Dotenv::createImmutable($_SESSION["dot_env_path"]);
     $dotenv->load();
 }
+header("Access-Control-Allow-Origin: localhost:3000");
