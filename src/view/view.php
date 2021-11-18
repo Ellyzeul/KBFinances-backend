12496267828296462fee5b6ad79e6c245f40817c
Ellyzeul <gabrielaug.240@gmail.com>
2021-11-17 22:42:06 -0300

    feat: adding headers to response

--- src/view/view.php
+++ src/view/view.php
@@ -7,6 +7,9 @@ class View
     {
         $json = json_encode($response);
         header('Content-Type: application/json; charset=utf-8');
+        header("Access-Control-Allow-Origin: http://localhost:3000");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-type");
         http_response_code($status_code);
 
         echo $json;
