c29ab074570d8c6bdd6a46b0c486f1d70cf7e682
Ellyzeul <gabrielaug.240@gmail.com>
2022-02-14 21:52:46 -0300

    chore: Handle CORS for netlify production host

--- config.php
+++ config.php
@@ -4,9 +4,23 @@ if(!isset($_ENV["DB_NAME"])) {
     $dotenv = Dotenv\Dotenv::createImmutable($_SESSION["dot_env_path"]);
     $dotenv->load();
 }
-
+$allowedOrigins = [
+    "kbfinances.netlify.app"
+];
+var_dump($_SERVER);
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: http://localhost:3000, https://kbfinances.netlify.app");
+    $origin = $_SERVER['HTTP_ORIGIN'];
+    $protocol = $_SERVER['HTTP_PROTOCOL'];
+
+    if($origin == 'localhost') {
+        $port = $_SERVER['SERVER_PORT'];
+        header("Access-Control-Allow-Origin: http://localhost:$port");
+    }
+    foreach($allowedOrigins as $allowed) {
+        if($origin == $allowed) {
+            header("Access-Control-Allow-Origin: $protocol://$origin");
+        }
+    }
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-type");
     http_response_code(204);
