87ae2007c780d14a5e41fb4c8967b78d25fd279d
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2022-02-16 18:12:32 -0300

    Update config.php

--- config.php
+++ config.php
@@ -5,12 +5,11 @@ if(!isset($_ENV["DB_NAME"])) {
     $dotenv->load();
 }
 
-$allowed = ["http:/localhost:3000/", "https://kbfinances.netlify.app/"];
+$allowed = ["http:/localhost:3000//", "https://kbfinances.netlify.app/"];
 $origin = $_SERVER['HTTP_REFERER'];
 
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    //if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
-    header("Access-Control-Allow-Origin: http://localhost:3000");
+    if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-type");
     http_response_code(204);
