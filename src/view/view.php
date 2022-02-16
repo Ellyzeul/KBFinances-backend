ee355b2819932f10dd639da0924aeec9b31655e5
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2022-02-16 18:13:36 -0300

    Update view.php

--- src/view/view.php
+++ src/view/view.php
@@ -8,11 +8,10 @@ class View
         $json = json_encode($response);
         header('Content-Type: application/json; charset=utf-8');
         
-        $allowed = ["http:/localhost:3000/", "https://kbfinances.netlify.app/"];
+        $allowed = ["http://localhost:3000/", "https://kbfinances.netlify.app/"];
         $origin = $_SERVER['HTTP_REFERER'];
 
-        // if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
-        header("Access-Control-Allow-Origin: http://localhost:3000");
+        if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
         header("Access-Control-Allow-Origin: https://kbfinances.netlify.app");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         header("Access-Control-Allow-Headers: Content-type");
