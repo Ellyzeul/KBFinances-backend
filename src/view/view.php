7125ce43436f88b89960e918695456b4314559b6
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2022-02-16 18:19:15 -0300

    Update view.php

--- src/view/view.php
+++ src/view/view.php
@@ -12,7 +12,6 @@ class View
         $origin = $_SERVER['HTTP_REFERER'];
 
         if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
-        header("Access-Control-Allow-Origin: https://kbfinances.netlify.app");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         header("Access-Control-Allow-Headers: Content-type");
         http_response_code($status_code);
