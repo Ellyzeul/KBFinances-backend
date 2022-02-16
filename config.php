cb0c823d280e7e7ed0165495d46083bb3b42eb5f
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2022-02-16 12:45:05 -0300

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ if(!isset($_ENV["DB_NAME"])) {
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: https://kbfinances.netlify.app/");
+    header("Access-Control-Allow-Origin: https://kbfinances.netlify.app");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-type");
     http_response_code(204);
