d321ac01d8f72f4649aa5d27ae1045ea0c2e7740
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2022-02-16 17:40:19 -0300

    Update view.php

--- src/view/view.php
+++ src/view/view.php
@@ -11,7 +11,8 @@ class View
         $allowed = ["http:/localhost:3000/", "https://kbfinances.netlify.app/"];
         $origin = $_SERVER['HTTP_REFERER'];
 
-        if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
+        // if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
+        header("Access-Control-Allow-Origin: http://localhost:3000");
         header("Access-Control-Allow-Origin: https://kbfinances.netlify.app");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         header("Access-Control-Allow-Headers: Content-type");
