ecd4781bbecbc4a96d9d581edcd5da01005a0960
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2022-02-16 11:15:00 -0300

    fix: CORS update

--- config.php
+++ config.php
@@ -5,8 +5,11 @@ if(!isset($_ENV["DB_NAME"])) {
     $dotenv->load();
 }
 
+$allowed = ["http://localhost:3000", "https://kbfinances.netlify.app"];
+$origin = $_SERVER["HTTP_ORIGIN"];
+
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("Access-Control-Allow-Origin: http://localhost:3000");
+    if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: $origin");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-type");
     http_response_code(204);
