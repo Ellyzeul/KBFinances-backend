2fea795a25f11618e0b8039ffde4d07e9550f496
Gabriel Augusto <55140361+Ellyzeul@users.noreply.github.com>
2022-02-16 17:39:51 -0300

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,8 @@ $allowed = ["http:/localhost:3000/", "https://kbfinances.netlify.app/"];
 $origin = $_SERVER['HTTP_REFERER'];
 
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
+    //if(in_array($origin, $allowed)) header("Access-Control-Allow-Origin: " . substr($origin, 0, -1));
+    header("Access-Control-Allow-Origin: http://localhost:3000");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
     header("Access-Control-Allow-Headers: Content-type");
     http_response_code(204);
